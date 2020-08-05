8865ebdae154b6d71831fa3953c7ff96509044c8
Fernando Fernandes <fernando.fernandes@labtwin.com>
2020-08-05 12:45:01 +0200

    Introduce asyncAfter

--- Sources/Worker/Worker.swift
+++ Sources/Worker/Worker.swift
@@ -3,21 +3,34 @@ import Foundation
 /// Executes code in background threads or in the main thread.
 public struct Worker {
 
-    /// Runs code in a background thread (`.userInitiated`) asynchronously.
+    /// Runs code in the main thread asynchronously (`DispatchQueue.asyncAfter...` or `DispatchQueue.async...`).
     ///
-    /// - Parameter backgroundWork: The code block to be executed in the background thread.
-    public static func doBackgroundWork(_ backgroundWork: @escaping () -> Void) {
-        DispatchQueue.global(qos: .userInitiated).async {
-            backgroundWork()
+    /// - Parameter mainThreadWork: The code block to be executed in the main thread.
+    public static func doMainThreadWork(after time: Double? = nil, mainThreadWork: @escaping () -> Void) {
+        if let givenTime = time {
+            DispatchQueue.main.asyncAfter(deadline: .now() + givenTime) {
+                mainThreadWork()
+            }
+        } else {
+            DispatchQueue.main.async {
+                mainThreadWork()
+            }
         }
     }
 
-    /// Runs code in the main thread asynchronously (`DispatchQueue.main.async`).
+    /// Runs code in a background thread asynchronously (`DispatchQueue.asyncAfter...` or `DispatchQueue.async...`. using
+    /// (`.userInitiated`) quality-of-service.
     ///
-    /// - Parameter mainThreadWork: The code block to be executed in the main thread.
-    public static func doMainThreadWork(_ mainThreadWork: @escaping () -> Void) {
-        DispatchQueue.main.async {
-            mainThreadWork()
+    /// - Parameter backgroundWork: The code block to be executed in the background thread.
+    public static func doBackgroundWork(after time: Double? = nil, backgroundWork: @escaping () -> Void) {
+        if let givenTime = time {
+            DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + givenTime) {
+                backgroundWork()
+            }
+        } else {
+            DispatchQueue.global(qos: .userInitiated).async {
+                backgroundWork()
+            }
         }
     }
 }
